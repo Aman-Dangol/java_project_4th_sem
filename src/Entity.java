cb6e200fbabfb2f52bcbea4702dcecdf8d521ee9
Aman Dangol <amandangol423@gmail.com>
2023-08-05 13:38:13 +0545

    added image

--- src/Entity.java
+++ src/Entity.java
@@ -1,6 +1,13 @@
+import java.awt.image.BufferedImage;
+
 public class Entity {
     int x ;
     int y;
-    int speed=4;
+    int speed=5;
+    int fallingSpeed = 10;
+    int jump =-3;
+    String direction;
+
+    BufferedImage left1,left2,right1,right2;
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,36 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity{
     GamePanel gamePanel;
+
+    String prevDirection="right";
+
     Player(GamePanel gp){
         x=100;
         y=100;
         gamePanel=gp;
+        getPlayerImage();
+        direction="right";
+    }
+    void getPlayerImage(){
+        try {
+            left1= ImageIO.read(getClass().getResourceAsStream("/player/left1.png"));
+            left2=ImageIO.read(getClass().getResourceAsStream("/player/left-2.png"));
+            right1=ImageIO.read(getClass().getResourceAsStream("/player/right-1.png"));
+            right2=ImageIO.read(getClass().getResourceAsStream("/player/right-2.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     void update(String direction){
+        this.direction=direction;
         if(direction=="up"){
-            y-=speed;
+            jump();
         }
         if(direction=="down"){
             y+=speed;
@@ -23,18 +43,45 @@ public class Player extends Entity{
         }
 
     }
+    void jump(){
+        fallingSpeed=jump;
+        y+=fallingSpeed;
+    }
     void falling(){
-        if(y<gamePanel.height-gamePanel.tileSize){
-            y += 2;
+        if(y<gamePanel.height-gamePanel.tileSize) {
+            y += fallingSpeed;
+            fallingSpeed++;
+
         }
-        else
-            y= gamePanel.height-gamePanel.tileSize;
-        System.out.println(y);
+        else {
+            y = gamePanel.height - gamePanel.tileSize;
+        }
 
     }
     void draw(Graphics2D g2d){
-        g2d.fillRect(x,y, gamePanel.tileSize, gamePanel.tileSize);
-        g2d.dispose();
+        BufferedImage image = null;
+        switch (direction){
+            case "up":
+
+                if (prevDirection=="left"){
+                    image=left1;
+                }else if (prevDirection=="right"){
+                    image=right2;
+                }
+
+                break;
+            case "left":
+                image=left1;
+                prevDirection="left";
+                break;
+            case "right":
+                image=right1;
+                prevDirection="right";
+
+                break;
+
+        }
+        g2d.drawImage(image,x,y,gamePanel.tileSize, gamePanel.tileSize, null);
     }
 
 }
