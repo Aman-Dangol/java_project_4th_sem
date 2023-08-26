6492a75d9b76e1833c6b8feefb1ec05ca279caad
Aman Dangol <amandangol423@gmail.com>
2023-08-26 18:54:32 +0545

    down left right done. jump left

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -57,7 +57,7 @@ public class CollisionChecker {
             case "left":
                 entityLeftCol=(entityLeftWorldX-entity.speed)/gp.tileSize;
                 tileNum1=gp.tileM.mapTileNum[entityLeftCol][entityTopRow];
-                tileNum2=gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
+                tileNum2=gp.tileM.mapTileNum[entityLeftCol][entityTopRow];
                 if (gp.tileM.tile[tileNum1].collision==true || gp.tileM.tile[tileNum2].collision==true){
                     System.out.println("left");
 
@@ -67,7 +67,7 @@ public class CollisionChecker {
             case "right":
                 entityRightCol=(entityRightWorldX+entity.speed)/gp.tileSize;
                 tileNum1=gp.tileM.mapTileNum[entityRightCol][entityTopRow];
-                tileNum2=gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
+                tileNum2=gp.tileM.mapTileNum[entityRightCol][entityTopRow];
                 if (gp.tileM.tile[tileNum1].collision==true || gp.tileM.tile[tileNum2].collision==true){
                     entity.collisionOn=true;
                 }
