650aedd9a6455fda0a4c0b22d5b9e93fee54102e
Aman Dangol <amandangol423@gmail.com>
2023-09-16 20:28:58 +0545

    final commit

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -20,19 +20,19 @@ public class AssetSetter {
     public void setEnemy(int enemyNum){
         for (int i = 0; i < enemyNum; i++) {
             gp.enemy[i] = new Enemy1(gp);
-//            boolean place= false;
-//            int x=0,y=0;
-//            while (!place){
-//                x = random.nextInt(1,gp.maxWorldCol)-1;
-//                y = random.nextInt(1,gp.maxWorldRow)-1;
-//                int tileNum = gp.tileM.mapTileNum[x][y];
-//                if (!gp.tileM.tile[tileNum].collision){
-//                    place=true;
-//                }
-//
-//            }
-            gp.enemy[i].worldX = gp.tileSize*10;
-            gp.enemy[i].worldY = gp.tileSize*10;
+            boolean place= false;
+            int x=0,y=0;
+            while (!place){
+                x = random.nextInt(0,gp.maxWorldCol-1);
+                y = random.nextInt(0,gp.maxWorldRow-1);
+                int tileNum = gp.tileM.mapTileNum[x][y];
+                if (!gp.tileM.tile[tileNum].collision){
+                    place=true;
+                }
+
+            }
+            gp.enemy[i].worldX = gp.tileSize*x;
+            gp.enemy[i].worldY = gp.tileSize*y;
             System.out.println(gp.enemy[i].worldX +" "+gp.enemy[i].worldY);
         }
 
